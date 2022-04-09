85976da5d71a3fa13c992ddd843e83782401af08
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-04-09 13:51:36 -0400

    Create userHandlers.js
    Initial commit

---
+++ userHandlers.js
@@ -0,0 +1,16 @@
+let users = [];
+
+const userHandlers = {
+  addUser: async (client) => {
+    users.push(client.chatName);
+    console.log(users2);
+  },
+  removeUser: async (client) => {
+    users = users.filter((e) => e !== client);
+  },
+  exists: async (client) => {
+    if (users.includes(client.chatName)) return true;
+    else return false;
+  },
+};
+module.exports = { userHandlers };
